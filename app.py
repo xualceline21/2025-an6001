9131927414229a4ae7b4c1b73a9410853a1330ad
xualceline21 <xual.celine21@gmail.com>
2025-11-21 02:35:44 +0000

    1

--- app.py
+++ app.py
@@ -12,5 +12,11 @@ def main():
     print(q)
     return(render_template("main.html"))
 
+@app.route("/dbs", methods = ["GET", "POST"])
+def dbs():
+    q = request.form.get("q")
+    print(q)
+    return(render_template("dbs.html"))
+
 if __name__ == "__main__":
     app.run()
